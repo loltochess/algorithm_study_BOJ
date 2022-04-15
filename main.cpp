c58ceb9b5e6ad8feb6d9b9a666346471bedce072
kimseojin <gg4ever1724@gmail.com>
2022-04-15 17:03:31 +0900

    big_number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
-    cout<<"DRX_FIGHTING"<<endl;
+    int n,m,k;
+    cin>>n>>m>>k;
+    int data[n]={0};
+    for(int i=0;i<n;i++){
+        cin>>data[i];
+    }
+    sort(data,data+n);
+    int sum=0;
+    int count=0;
+    int first=data[n-1];
+    int second=data[n-2];
+    
+    count=m/(k+1)*k;
+    count+=m%(k+1);
+
+    sum+=count*first;
+    sum+=(m-count)*second;
+    
+    cout<<sum<<endl;
 }
